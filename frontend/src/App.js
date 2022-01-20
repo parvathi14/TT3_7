6da10a014e00f43d0e3ed2531b1de08e358656ad
GuoCong96 <guo_cong-L.124@hotmail.com>
2022-01-20 14:11:42 +0800

    commiting navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+import { Navbar } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App = () => {
+    
+}
+
+export default App;
